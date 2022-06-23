e681438c513d0d3d76c92181f07f64ac4bc56d16
liteng5 <liteng5@chanjet.com>
2022-06-23 21:06:51 +0800

    zookeeper项目初始化

---
+++ zookeeper-client-original/src/main/java/com/stockeeper/zookeeper/ZookeeperClientOriginalApplication.java
@@ -0,0 +1,18 @@
+package com.stockeeper.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jyinjuly
+ * @version 2022-06-23 19:55
+ * @description TODO
+ */
+@SpringBootApplication
+public class ZookeeperClientOriginalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperClientOriginalApplication.class, args);
+    }
+
+}
---
+++ zookeeper-client-original/src/test/java/com/stockeeper/zookeeper/ZookeeperClientOriginalApplicationTests.java
@@ -0,0 +1,47 @@
+package com.stockeeper.zookeeper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author jyinjuly
+ * @version 2022-06-23 16:52
+ * @description TODO
+ */
+@Slf4j
+@SpringBootTest
+public class ZookeeperClientOriginalApplicationTests {
+
+    private static final String ZOOKEEPER_SERVER_ADDRESS = "192.168.175.175:2181";
+    private static final int SESSION_TIMEOUT = 5000;
+    private static ZooKeeper zookeeper = null;
+
+    @BeforeEach
+    void init() throws IOException, InterruptedException {
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        zookeeper = new ZooKeeper(ZOOKEEPER_SERVER_ADDRESS, SESSION_TIMEOUT, event -> {
+            if (event.getState() == Watcher.Event.KeeperState.SyncConnected && event.getType() == Watcher.Event.EventType.None) {
+                log.info("Zookeeper连接成功.");
+                countDownLatch.countDown();
+            }
+        });
+        log.info("Zookeeper连接中......");
+        countDownLatch.await();
+    }
+
+    @Test
+    void test() {
+        log.info("123123");
+    }
+
+
+
+
+}
